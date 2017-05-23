43f6f89d85ccc189d89cf864b8443d92204270e3
GioPetrucci <diazz.george@icloud.com>
2017-05-23 18:30:06 -0500

    Subir archivos para prueba

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp')
+var postcss = require('gulp-postcss')
+var cssnext = require('postcss-cssnext')
+var cssnested = require('postcss-nested')
+var mixins = require('postcss-mixins')
+var lost = require('lost')
+var atImport = require('postcss-import')
+var csswring = require('csswring')
+var mqpacker = require('css-mqpacker')
+var browserSync = require('browser-sync').create()
+
+//Servidor de desarrollo
+gulp.task('serve', function() {
+	browserSync.init({
+		server: {
+			baseDir: './dist'
+		}
+	})
+})
+
+//Tarea para procesar el CSS
+gulp.task('css', function (){
+
+	var processors = [
+	atImport(),
+	mixins(),
+	cssnested,
+	lost(),
+	// rucksack,
+	cssnext({ browsers: ['>5%', 'ie 8']}),
+	mqpacker,
+	csswring()
+	 ]
+
+	return gulp.src('./src/*.css')
+	return gulp.src('./src/*.html')
+	.pipe(postcss(processors))
+	.pipe(gulp.dest('./dist/css'))
+	.pipe(browserSync.stream())
+})
+
+
+//Tarea para vigilar los cambios
+gulp.task('watch', function() {
+	gulp.watch('./dist/*.css', ['css'])
+	// gulp.watch('./src/*.html').on('change', browserSync.reload) así estaba antes
+	gulp.watch('./dist/*.html').on('change', browserSync.reload)
+})
+
+//Unir las tareas
+gulp.task('default', ['watch', 'serve', 'css'])
